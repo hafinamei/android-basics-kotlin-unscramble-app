975f2dab4b0446298f10beb40301ac1bbf7584ca
Ersado223307039 <ersadocb2505@gmail.com>
2023-09-25 14:32:57 +0700

    melampirkan observer untuk score

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -69,6 +69,10 @@ class GameFragment : Fragment() {
             { newWord ->
                 binding.textViewUnscrambledWord.text = newWord
             })
+        viewModel.score.observe(viewLifecycleOwner,
+            { newScore ->
+                binding.score.text = getString(R.string.score, newScore)
+            })
 
     }
 
