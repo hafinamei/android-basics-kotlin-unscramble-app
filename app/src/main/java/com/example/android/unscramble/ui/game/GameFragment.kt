c3f929c61260d54bf097b0ce8bfcba7110e055fb
hafinamei <hafina.meilawati@gmail.com>
2023-09-25 15:11:10 +0700

    Menambahkan variabel data binding

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -58,6 +58,9 @@ class GameFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.gameViewModel = viewModel
+
+        binding.maxNoOfWords = MAX_NO_OF_WORDS
 
 
 
